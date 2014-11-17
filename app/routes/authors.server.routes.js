5b8632829bbc896a6c2367d650e67f3052d6a143
Recvi <th.charalampos@gmail.com>
2014-11-17 14:50:31 +0200

    Fixed .gitignore file and commented author routes till finished.

--- app/routes/authors.server.routes.js
+++ app/routes/authors.server.routes.js
@@ -2,13 +2,13 @@
 
 
 module.exports = function(app) {
-    // User Routes
-    var users = require('../../app/controllers/users');
+    // // User Routes
+    // var users = require('../../app/controllers/users');
 
-    // Setting the authors api.
-    app.route('authors').get(authors.list);
-    app.route('authors:authorId').get(authors.profile);
+    // // Setting the authors api.
+    // app.route('authors').get(authors.list);
+    // app.route('authors:authorId').get(authors.profile);
 
-    // Finish by binding the user middleware
-    app.param('authorId', users.userByID);
-}
+    // // Finish by binding the user middleware
+    // app.param('authorId', users.userByID);
+};
