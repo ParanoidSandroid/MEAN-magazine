a9def88fc7759caa8537689d828d23e37b52c779
Recvi <th.charalampos@gmail.com>
2014-11-23 23:21:03 +0200

    Modify module categories routes to include visitor states.

--- public/modules/categories/config/categories.client.routes.js
+++ public/modules/categories/config/categories.client.routes.js
@@ -2,24 +2,30 @@
 
 //Setting up route
 angular.module('categories').config(['$stateProvider',
-	function($stateProvider) {
-		// Categories state routing
-		$stateProvider.
-		state('listCategories', {
-			url: '/categories',
-			templateUrl: 'modules/categories/views/list-categories.client.view.html'
-		}).
-		state('createCategory', {
-			url: '/categories/create',
-			templateUrl: 'modules/categories/views/create-category.client.view.html'
-		}).
-		state('viewCategory', {
-			url: '/categories/:categoryId',
-			templateUrl: 'modules/categories/views/view-category.client.view.html'
-		}).
-		state('editCategory', {
-			url: '/categories/:categoryId/edit',
-			templateUrl: 'modules/categories/views/edit-category.client.view.html'
-		});
-	}
+    function($stateProvider) {
+        // Categories state routing
+        $stateProvider.
+            // User states
+        state('listCategories', {
+            url: '/manage/categories',
+            templateUrl: 'modules/categories/views/list-categories.client.view.html'
+        }).
+        state('createCategory', {
+            url: '/manage/categories/create',
+            templateUrl: 'modules/categories/views/create-category.client.view.html'
+        }).
+        state('viewCategory', {
+            url: '/manage/categories/:categoryId',
+            templateUrl: 'modules/categories/views/view-category.client.view.html'
+        }).
+        state('editCategory', {
+                url: '/manage/categories/:categoryId/edit',
+                templateUrl: 'modules/categories/views/edit-category.client.view.html'
+        }).
+        // Visitor states
+        state('visitCategory', {
+            url: '/categories/:categoryId',
+            templateUrl: 'modules/categories/views/visit-category.client.view.html'
+        });
+    }
 ]);
