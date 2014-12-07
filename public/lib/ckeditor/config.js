747a8664dcde4062289d2d64be8818ba528b4d93
Recvi <th.charalampos@gmail.com>
2014-12-08 01:10:02 +0200

    Removed autogrow, fow now, till we figure out a better solution.

--- public/lib/ckeditor/config.js
+++ public/lib/ckeditor/config.js
@@ -7,7 +7,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-	config.extraPlugins = 'widget,lineutils,image2,panel,button,listblock,floatpanel,richcombo,font,ckeditor-gwf-plugin,autogrow';
+	config.extraPlugins = 'widget,lineutils,image2,panel,button,listblock,floatpanel,richcombo,font,ckeditor-gwf-plugin';
 	config.font_names = "Arial/Arial, Helvetica, sans-serif;Comic Sans MS/Comic Sans MS, cursive;Courier New/Courier New, Courier, monospace;Georgia/Georgia, serif;Lucida Sans Unicode/Lucida Sans Unicode, Lucida Grande, sans-serif;Tahoma/Tahoma, Geneva, sans-serif;Times New Roman/Times New Roman, Times, serif;Trebuchet MS/Trebuchet MS, Helvetica, sans-serif;Verdana/Verdana, Geneva, sans-serif;Cambria, Georgia, serif;CenturyGothic, AppleGothic, sans-serif;GoogleWebFonts;";
 ;
 };
