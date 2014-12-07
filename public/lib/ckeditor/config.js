539e8b223d06ff205ec3bd97291845b4090c32cb
ParanoidSandroid <sanampelo@yahoo.gr>
2014-12-07 22:54:59 +0200

    CKEditor: Added plugin for fonts + google web fonts

--- public/lib/ckeditor/config.js
+++ public/lib/ckeditor/config.js
@@ -7,5 +7,6 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-	config.extraPlugins = 'widget,lineutils,image2';
+	config.extraPlugins = 'widget,lineutils,image2,panel,button,listblock,floatpanel,richcombo,font,ckeditor-gwf-plugin_0.1.1';
+	config.font_names = 'GoogleWebFonts;'
 };
