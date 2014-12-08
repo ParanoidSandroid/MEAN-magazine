aa6e53d83d3a438602fb1e674ebf89090f70afd7
ParanoidSandroid <sanampelo@yahoo.gr>
2014-12-09 01:58:02 +0200

    Fixed previous omissions.

--- public/modules/users/controllers/authors.client.controller.js
+++ public/modules/users/controllers/authors.client.controller.js
@@ -3,4 +3,5 @@
 angular.module('users').controller('AuthorsController', ['$scope', '$stateParams', 'Authors',
 	function($scope, $stateParams, Authors) {
 		$scope.author = Authors.get({authorId: $stateParams.authorId});
-	}])
+	}
+]);
