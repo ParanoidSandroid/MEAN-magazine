e9c60acd4854cdb0ded0d5a41fe0965f42d0850c
ParanoidSandroid <sanampelo@yahoo.gr>
2014-12-09 15:41:19 +0200

    Added list of authors in 'about' page.

--- public/modules/users/controllers/authors.client.controller.js
+++ public/modules/users/controllers/authors.client.controller.js
@@ -3,5 +3,9 @@
 angular.module('users').controller('AuthorsController', ['$scope', '$stateParams', 'Authors',
 	function($scope, $stateParams, Authors) {
 		$scope.author = Authors.get({authorId: $stateParams.authorId});
+
+		$scope.find = function() {
+			$scope.authors = Users.query();
+		};
 	}
 ]);
