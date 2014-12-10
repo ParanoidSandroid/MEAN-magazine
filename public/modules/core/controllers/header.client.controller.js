a9f65bd190f9b3ac2bde0e0b504d1483c18bb4f8
Recvi <th.charalampos@gmail.com>
2014-12-10 04:26:26 +0200

    Reversed categories array to properly show categories in navbar.

--- public/modules/core/controllers/header.client.controller.js
+++ public/modules/core/controllers/header.client.controller.js
@@ -3,7 +3,9 @@
 angular.module('core').controller('HeaderController', ['$scope', 'Authentication', 'Menus', 'Categories',
     function($scope, Authentication, Menus, Categories) {
         $scope.authentication = Authentication;
-        $scope.categories = Categories.query();
+        $scope.categories = Categories.query(function() {
+            $scope.categories = $scope.categories.reverse();
+        });
         $scope.isCollapsed = false;
         $scope.menu = Menus.getMenu('topbar');
 
