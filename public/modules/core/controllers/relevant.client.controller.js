98859f49855b305cfd9b8dd09d83de759ab2e693
ParanoidSandroid <sanampelo@yahoo.gr>
2014-12-12 22:34:21 +0200

    ---> Fixed relevant controller to order the articles correctly. !A TypeError remains! (-_-)

--- public/modules/core/controllers/relevant.client.controller.js
+++ public/modules/core/controllers/relevant.client.controller.js
@@ -7,17 +7,18 @@ angular.module('core').controller('RelevantController', ['$scope', '$stateParams
             articleId: $stateParams.articleId
         }, function() {
             //Get all the articles that belong to the same category
-            $scope.relevants = Articles.query({category : $scope.article.category._id});
-            var _ = $window._;
-            for(var a in $scope.relevants) {
-                var mutual_tags = _.intersection(_.pluck($scope.relevants[a].tags, '_id'), _.pluck($scope.article.tags, '_id'));
-                if (mutual_tags.length > 0) {
-                    $scope.relevants[a].relativity = mutual_tags.length;
-                }
-                else {
-                    $scope.relevants.splice(a,1);
-                }
-            }
+            $scope.relevants = Articles.query({category : $scope.article.category._id}, function() {
+                var _ = $window._;
+                for(var a in $scope.relevants) {
+                    var mutual_tags = _.intersection(_.pluck($scope.relevants[a].tags, '_id'), _.pluck($scope.article.tags, '_id'));
+                    if (mutual_tags.length > 0) {
+                        $scope.relevants[a].relativity = mutual_tags.length;
+                    }
+                    else {
+                        $scope.relevants[a].relativity = 0;
+                    }
+                }  
+            });
         });
     }
 ]);
