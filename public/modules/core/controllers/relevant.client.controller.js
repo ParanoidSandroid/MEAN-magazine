b1b919f67d899fba0fcae5ffe02515b4a2fbc67c
ParanoidSandroid <sanampelo@yahoo.gr>
2014-12-12 21:16:48 +0200

    Created relevant articles controller and fixed a bunch of stuff  ~(ΟωΟ)~

---
+++ public/modules/core/controllers/relevant.client.controller.js
@@ -0,0 +1,23 @@
+'use strict';
+
+
+angular.module('core').controller('RelevantController', ['$scope', '$stateParams', '$window', 'Articles',
+    function($scope, $stateParams, $window, Articles) {
+        $scope.article = Articles.get({
+            articleId: $stateParams.articleId
+        }, function() {
+            //Get all the articles that belong to the same category
+            $scope.relevants = Articles.query({category : $scope.article.category._id});
+            var _ = $window._;
+            for(var a in $scope.relevants) {
+                var mutual_tags = _.intersection(_.pluck($scope.relevants[a].tags, '_id'), _.pluck($scope.article.tags, '_id'));
+                if (mutual_tags.length > 0) {
+                    $scope.relevants[a].relativity = mutual_tags.length;
+                }
+                else {
+                    $scope.relevants.splice(a,1);
+                }
+            }
+        });
+    }
+]);
